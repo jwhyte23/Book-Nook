d810923e6705ee214019cea59b464a1a48e693a1
jwhyte23 <64270160+jwhyte23@users.noreply.github.com>
2026-05-17 18:34:55 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'book-nook-v1';
+const CACHE_NAME = 'book-nook-v2';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 // Install: cache all assets
