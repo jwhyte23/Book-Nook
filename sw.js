29416993355ffab69a70a064a757370bf54b0d79
jwhyte23 <64270160+jwhyte23@users.noreply.github.com>
2026-05-20 11:08:40 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'book-nook-v3';
+const CACHE_NAME = 'book-nook-v4';
 const STATIC_ASSETS = ['./icon-192.png', './icon-512.png', './apple-touch-icon.png', './manifest.json'];
 
 // Install: pre-cache only static assets (not the HTML)
